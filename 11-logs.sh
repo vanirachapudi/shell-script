ab25ce80dc325c54a938573c7c51aafe40566679
vani0432 <grandhivani@gmail.com>
2024-03-12 21:58:34 +0530

    msg

--- 11-logs.sh
+++ 11-logs.sh
@@ -12,17 +12,17 @@ VALIDATE(){
 
     if [ $1 -ne 0 ]
     then
-        echo "ERROR:: $2 ... $R FAILED $N"
+        echo  -e "ERROR:: $2 ... $R FAILED $N"
         exit 1
     else
-        echo "$2 .....  $R SUCCESS $N"  
+        echo  -e "$2 .....  $R SUCCESS $N"  
     fi 
 
     }
 
 if [ $ID -ne 0 ]
 then
-    echo "$R ERROR:: please run this script with root access $N"
+    echo -e  "$R ERROR:: please run this script with root access $N"
     exit 1
 else
     echo "your root user"
