e9b7158dacc1581dd8dcbf1931c11e6358a3cdc5
vani0432 <grandhivani@gmail.com>
2024-03-12 21:57:13 +0530

    msg

--- 11-logs.sh
+++ 11-logs.sh
@@ -2,23 +2,27 @@
 
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
 LOGFILE="/temp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
 
     if [ $1 -ne 0 ]
     then
-        echo "ERROR:: $2 ... FAILED"
+        echo "ERROR:: $2 ... $R FAILED $N"
         exit 1
     else
-        echo "$2 .....  SUCCESS"  
+        echo "$2 .....  $R SUCCESS $N"  
     fi 
 
     }
 
 if [ $ID -ne 0 ]
 then
-    echo "ERROR:: please run this script with root access"
+    echo "$R ERROR:: please run this script with root access $N"
     exit 1
 else
     echo "your root user"
